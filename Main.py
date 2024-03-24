1ba36f2c361c17d24295fc3af441fe138bcb273b
Greg Carette <grcarette1@gmail.com>
2024-03-24 11:10:32 -0400

    fixed girder issue

--- Main.py
+++ Main.py
@@ -4,8 +4,8 @@ from AutoOptimizer import AutoOptimizer
 from ArrayPlotter import ArrayPlotter
 import numpy as np
        
-def generate_level(array_plotter):
-    level_grid = LevelGrid([4,7], array_plotter)
+def generate_level(array_plotter, test_display, map_size):
+    level_grid = LevelGrid(array_plotter, test_display, map_size)
     level_grid.Generate_Level()
     optimize_level(level_grid, array_plotter)
     
@@ -16,15 +16,14 @@ def optimize_level(level_grid,array_plotter):
     generate_map(optimizer.block_list,boundaries, array_plotter, level_grid)
 
 def generate_map(block_list, boundaries, array_plotter, level_grid):
-    new_level = LevelGenerator(boundaries, level_grid.startgoal_pos)
+    new_level = LevelGenerator(boundaries, level_grid)
     new_level.Generate_Level(block_list)
 
 if __name__ == "__main__":
+    test_display = 0
     array_plotter = ArrayPlotter()
     for i in range(1):
-        generate_level(array_plotter)
-    # test_arr = np.ones([9,9])
-    # optimize_level(test_arr)
+        generate_level(array_plotter, test_display, 'l')
 
 
     
